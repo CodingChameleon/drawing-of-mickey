396de41586b0970ea8db50de5992035d50dc6412
Naia McNatt <nmcnatt@wlapcs.org>
2017-03-02 16:52:53 +0000

    Yay

---
+++ sketch.js
@@ -0,0 +1,38 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    fill(0,0,0);
+    ellipse(180,75,150,150);
+    ellipse(380,75,150,150);
+    ellipse(280,200,200,200);
+    
+    stroke(255,225,205)
+    fill(255,225,205);
+    ellipse(280,240,175,120);
+    ellipse(254,160,55,100);
+    ellipse(309,160,55,100);
+    
+    
+    fill(255,255,255);
+    ellipse(260,180,40,70);
+    ellipse(300,180,40,70);
+    
+    stroke(255,255,255)
+    rect(125,235,10,10);
+    rect(100,210,30,30);
+    rect(50,160,70,70);
+    
+    fill(0,0,0);
+    ellipse(270,190,20,50);
+    ellipse(290,190,20,50);
+    
+    stroke(0,0,0);
+    strokeWeight(2);
+    line(235,215,327,215);
+    
+    fill(0,0,0);
+    ellipse(270,228,35,25);
+}
